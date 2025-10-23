fc350b96c993db39b19b25100c9f03024f24bd24
Misty123 <saideshna2210312@ssn.edu.in>
2025-10-23 23:26:05 +0530

    these are more changes

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { UpdateItemComponent } from './components/update-item/update-item.component';
 import { DeleteItemComponent } from './components/delete-items/delete-items.component';
+import { AddItemComponent } from './components/add-item/add-item.component';
+import { ViewItemsComponent } from './components/view-items/view-items.component';
 import { provideHttpClient } from '@angular/common/http';
 
 
@@ -11,6 +13,6 @@ import { provideHttpClient } from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [CommonModule, FormsModule, UpdateItemComponent, DeleteItemComponent]
+  imports: [CommonModule, FormsModule, AddItemComponent, ViewItemsComponent, UpdateItemComponent, DeleteItemComponent]
 })
 export class App {}
